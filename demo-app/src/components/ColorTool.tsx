4b052c5a97200705516100834e5a67bad822a448
Eric Greene <eric@t4d.io>
2020-04-27 17:04:42 -0400

    exercise 2

---
+++ demo-app/src/components/ColorTool.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const ColorTool = () => {
+  return (
+    <>
+      <header>
+        <h1>Color Tool</h1>
+      </header>
+      <ul>
+        <li>red</li>
+        <li>white</li>
+        <li>orange</li>
+        <li>brown</li>
+      </ul>
+    </>
+  );
+};
