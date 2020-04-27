3b07c8e960765758e70c237ccd56a44b5a3593eb
Eric Greene <eric@t4d.io>
2020-04-27 18:23:12 -0400

    add exercise 3

--- demo-app/src/components/ColorTool.tsx
+++ demo-app/src/components/ColorTool.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
 
+import { Color } from '../models/Color';
+
 export const ColorTool = () => {
+
+  const colors: Color[] = [
+    { id: 1, name: 'red' },
+    { id: 2, name: 'white' },
+    { id: 3, name: 'orange' },
+    { id: 4, name: 'brown' },
+  ];
+
+  const colorListItems = colors.map(color =>
+    <li key={color.id}>{color.name}</li>)
+
   return (
     <>
       <header>
         <h1>Color Tool</h1>
       </header>
       <ul>
-        <li>red</li>
-        <li>white</li>
-        <li>orange</li>
-        <li>brown</li>
+        {colorListItems}
       </ul>
     </>
   );
